3a165f1742f8d4777ec6689a3426d9b0201ba218
ArielWangX <109891752+ArielWangX@users.noreply.github.com>
2022-11-16 21:08:20 +1100

    Import csv file into SQLite database
    Import student csv file into database:
    * if the student id does not exist, import them all into database.
    * if there are some student id exist, skip these rows and import the student information row which student id does not exist. And output the student information of skipping rows as a table in console.

---
+++ Student_Management_System/HolmesglenStudentManagementSystem/HolmesglenStudentManagementSystem/BusinessLogicLayer/ImportStudentFromCSVBLL.cs
@@ -0,0 +1,44 @@
+﻿using HolmesglenStudentManagementSystem.DataAccessLayer;
+using HolmesglenStudentManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolmesglenStudentManagementSystem.BusinessLogicLayer
+{
+    public class ImportStudentFromCSVBLL
+    {
+        AppDAL appDAL;
+        public ImportStudentFromCSVBLL()
+        {
+            appDAL = new AppDAL();
+        }
+
+        public List<Student> ImportStudentToSQLite(List<Student> studentImport)
+        {
+            var studentBLL = new StudentBLL();
+            var studentExistList = new List<Student>();
+
+            foreach (var student in studentImport)
+            {
+                // if student id exists, add to studentExistList and skip to next iteration.
+                if (studentBLL.GetOne(student.Id) != null)
+                {
+                    studentExistList.Add(student);
+                    continue;                   
+                }
+                // if student id does not exist, create one row in sqlite student table.
+                else
+                {
+                    appDAL.StudentDALInstance.Create(student);
+                }               
+            }
+
+            // return studenExistList
+            return studentExistList;
+        }
+
+    } 
+}
---
+++ Student_Management_System/HolmesglenStudentManagementSystem/HolmesglenStudentManagementSystem/Models/ImportStudentFromCSV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolmesglenStudentManagementSystem.Models
+{
+    public class ImportStudentFromCSV
+    {     
+        public string StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public ImportStudentFromCSV() { }
+
+        public ImportStudentFromCSV(string studentID, string firstName, string lastName, string email)
+        {
+            StudentID = studentID;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
